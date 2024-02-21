8bcf8b24a84bd061425ce6ce9f463d776e1f54dc
Shukri Hussein <husseinshukri@outlook.com>
2024-02-21 12:32:53 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+//let customerName = upperCaseCustomerName('bob')
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+//bob === not bob
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'possibly bob'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'definately bob'
+    
+}
